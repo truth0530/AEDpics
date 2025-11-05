4fed139300a05025856b0d5c3ddc8d8f9ea6f7cb
carg <truth0530@gmail.com>
2025-11-05 23:02:14 +0900

    refactor: Update Roadview button icon to street view representation
    - Replace generic icon with building/panorama visualization icon
    - Icon represents the street view/panorama nature of Roadview
    - Maintains consistent styling with blue button and white text

--- components/inspection/steps/BasicInfoStep.tsx
+++ components/inspection/steps/BasicInfoStep.tsx
@@ -769,8 +769,11 @@ export function BasicInfoStep() {
             disabled={!isMapLoaded}
             title="로드뷰 보기"
           >
-            <svg className="w-3.5 h-3.5 flex-shrink-0" fill="currentColor" viewBox="0 0 24 24">
-              <path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 18c-4.42 0-8-3.58-8-8s3.58-8 8-8 8 3.58 8 8-3.58 8-8 8zm3.5-9c.83 0 1.5-.67 1.5-1.5S16.33 8 15.5 8 14 8.67 14 9.5s.67 1.5 1.5 1.5zm-7 0c.83 0 1.5-.67 1.5-1.5S9.33 8 8.5 8 7 8.67 7 9.5 7.67 11 8.5 11zm3.5 6.5c2.33 0 4.31-1.46 5.11-3.5H6.89c.8 2.04 2.78 3.5 5.11 3.5z"/>
+            <svg className="w-4 h-4 flex-shrink-0" fill="currentColor" viewBox="0 0 24 24">
+              <path d="M3 13h3v8H3z"/>
+              <path d="M9 5h3v16H9z"/>
+              <path d="M15 9h3v12h-3z"/>
+              <path d="M21 11h1v10h-1z"/>
             </svg>
             <span>로드뷰</span>
           </button>
